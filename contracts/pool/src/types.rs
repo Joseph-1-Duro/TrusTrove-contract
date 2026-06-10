6770aca1d885d84d85a801ac7efe365bb74b97fb
K1NGD4VID <adesanyafuhad5@gmail.com>
2026-06-10 22:32:02 +0100

    feat(pool): define PoolStats, LPPosition, DataKey types in types.rs

---
+++ contracts/pool/src/types.rs
@@ -0,0 +1,40 @@
+use soroban_sdk::{contracttype, Address, BytesN};
+
+#[contracttype]
+#[derive(Clone, Debug)]
+pub struct PoolStats {
+    pub total_deposits: u128,
+    pub total_funded: u128,
+    pub available_liquidity: u128,
+    pub utilization_rate_bps: u32,
+    pub total_yield_distributed: u128,
+    pub active_invoice_count: u32,
+    pub total_shares: u128,
+}
+
+#[contracttype]
+#[derive(Clone, Debug)]
+pub struct LPPosition {
+    pub shares: u128,
+    pub usdc_value: u128,
+    pub yield_earned: u128,
+    pub deposit_count: u32,
+}
+
+#[contracttype]
+pub enum DataKey {
+    Admin,
+    InvoiceContract,
+    EscrowContract,
+    UsdcAsset,
+    TotalShares,
+    TotalDeposits,
+    TotalFunded,
+    TotalYieldDistributed,
+    ActiveInvoiceCount,
+    LPShares(Address),
+    LPDepositCount(Address),
+    LPYieldEarned(Address),
+    LPInitialDeposit(Address),
+    FundedInvoice(BytesN<32>),
+}
