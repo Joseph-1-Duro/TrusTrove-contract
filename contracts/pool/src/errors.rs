cf36624eb80d5b120c7ab994a169bcc18b652da0
K1NGD4VID <adesanyafuhad5@gmail.com>
2026-06-10 22:32:17 +0100

    feat(pool): define PoolError contracterror enum in errors.rs

---
+++ contracts/pool/src/errors.rs
@@ -0,0 +1,16 @@
+use soroban_sdk::contracterror;
+
+#[contracterror]
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub enum PoolError {
+    AlreadyInitialized = 1,
+    NotFound = 2,
+    NotAuthorized = 3,
+    InvalidAmount = 4,
+    InsufficientLiquidity = 5,
+    NoShares = 6,
+    InsufficientShares = 7,
+    InvoiceNotListed = 8,
+    AlreadyFunded = 9,
+    InvoiceNotFound = 10,
+}
